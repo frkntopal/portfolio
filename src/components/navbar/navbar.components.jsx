33b9346e83eab8e2dfbd9466fc3711abccc5d158
Furkan Topal <frkntopall@outlook.com>
2023-06-04 20:09:59 +0300

    finished my portfolio

--- src/components/navbar/navbar.components.jsx
+++ src/components/navbar/navbar.components.jsx
@@ -1,14 +1,12 @@
 import './navbar.styles.scss';
 import { useState } from 'react';
 import Logo from '../../assets/logo.png'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-
-
 
 function Navbar() {
   const { t, i18n } = useTranslation();
-
+  const location = useLocation();
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   function toggleDropdown() {
@@ -23,47 +21,48 @@ function Navbar() {
     <header>
       <div className="navbar">
         <div className="logo">
-          <a href="#home">
+          <Link to="/">
             <img src={Logo} alt="Logo" />
-          </a>
+          </Link>
         </div>
-        <ul className="links">
-          
-          <li>
-            <a href="#projects">{t('Projects')}</a>
-          </li>
-          <li>
-            <a href="#contact">{t('Contact')}</a>
-          </li>
-          <li>
-            <a href="#information">{t('Information')}</a>
-          </li>
-        </ul>
-        <div className="toggle_btn" onClick={toggleDropdown}>
-          <i className="material-symbols-outlined">legend_toggle</i>
-        </div>
-        <div className={`dropdown_menu ${dropdownOpen ? 'open' : ''}`}>
-          <a className="dropdownmenu_content" href="#projects">
-            {t('Projects')}
-          </a>
-          <a className="dropdownmenu_content" href="#contact">
-            {t('Contact')}
-          </a>
-          <a className="dropdownmenu_content" href="#information">
-            {t('Information')}
-          </a>
-        </div>
-        <div className="checkbox-wrapper-55">
-          <label className="rocker rocker-small">
-            <input type="checkbox"/>
-            <span onClick={()=> changeLanguage('tr')} className="switch-left">TR</span>
-            <span onClick={()=> changeLanguage('en')} className="switch-right">EN</span>
-          </label>
-        </div>
+        {!location.pathname.includes('/blog/') && (
+          <>
+            <ul className="links">
+              <li>
+                <a href="#projects">{t('Projects')}</a>
+              </li>
+              <li>
+                <a href="#contact">{t('Contact')}</a>
+              </li>
+              <li>
+                <a href="#information">{t('Information')}</a>
+              </li>
+            </ul>
+            <div className="toggle_btn" onClick={toggleDropdown}>
+              <i className="material-symbols-outlined">legend_toggle</i>
+            </div>
+            <div className={`dropdown_menu ${dropdownOpen ? 'open' : ''}`}>
+              <a className="dropdownmenu_content" href="#projects">
+                {t('Projects')}
+              </a>
+              <a className="dropdownmenu_content" href="#contact">
+                {t('Contact')}
+              </a>
+              <a className="dropdownmenu_content" href="#information">
+                {t('Information')}
+              </a>
+            </div>
+            <div className="checkbox-wrapper-55">
+              <label className="rocker rocker-small">
+                <input type="checkbox"/>
+                <span onClick={()=> changeLanguage('tr')} className="switch-left">TR</span>
+                <span onClick={()=> changeLanguage('en')} className="switch-right">EN</span>
+              </label>
+            </div>
+          </>
+        )}
       </div>
-
     </header>
-    
   );
 }
 
